f7e603d6a801455c0696d7d15a4dd086534bf878
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2023-11-09 13:42:31 -0500

    added temporary cookie authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,34 @@
-import { Suspense } from "react";
+import React from "react";
+import { cookies } from "next/headers";
 import GlobalStateProvider from "@/lib/state/GlobalStateProvider";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
 
-const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <GlobalStateProvider>
-    <html lang="en">
-      <body>
-        <div className="min-h-screen flex">
-          <Sidebar />
-          <main className="flex-grow">
-            <Suspense fallback={<div>loading...</div>}>{children}</Suspense>
-          </main>
-        </div>
-      </body>
-    </html>
-  </GlobalStateProvider>
-);
+const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const accessToken = cookies().get(`access-token`);
+  if (!accessToken || accessToken.value !== process.env.ACCESS_TOKEN) {
+    return (
+      <html lang="en">
+        <body>
+          <div className="h-screen w-screen flex justify-center items-center">
+            access denied
+          </div>
+        </body>
+      </html>
+    );
+  }
+  return (
+    <GlobalStateProvider>
+      <html lang="en">
+        <body>
+          <div className="min-h-screen flex">
+            <Sidebar />
+            <main className="flex-grow">{children}</main>
+          </div>
+        </body>
+      </html>
+    </GlobalStateProvider>
+  );
+};
 
 export default RootLayout;
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,23 @@
+import { Suspense } from "react";
 import type { NextPage } from "next";
-import SignupGraph from "@/components/SignupGraph";
-import getAdminData from "@/lib/get-data";
+import SignupGraph from "@/components/SignupGraphContainer";
 
-const Home: NextPage = async () => {
-  const adminData = await getAdminData();
-  if (!adminData.success) {
-    return <div>{adminData.error}</div>;
-  }
-  return (
-    <main>
-      <header></header>
-      <div className="p-12">
-        <SignupGraph data={adminData.data} />
-        <div className="mt-8 grid gap-2"></div>
-      </div>
-    </main>
-  );
-};
+export const dynamic = `force-dynamic`;
+
+const Home: NextPage = async () => (
+  <main>
+    <header></header>
+    <div className="p-12">
+      <Suspense
+        fallback={
+          <div className="bg-slate-100 rounded-3xl h-96 animate-pulse border" />
+        }
+      >
+        <SignupGraph />
+      </Suspense>
+      <div className="mt-8 grid gap-2"></div>
+    </div>
+  </main>
+);
 
 export default Home;
